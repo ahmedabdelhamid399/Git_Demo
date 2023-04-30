48cade97c8d6ac2048a0ffb1ef40b948a4e516c4
Ahmed Abdelhamid <37750504+ahmedabdelhamid399@users.noreply.github.com>
2023-04-30 14:59:39 +0300

    Update HelloITI class with new method

--- HelloITI.java
+++ HelloITI.java
@@ -2,4 +2,7 @@ public class HelloITI {
     public static void printHelloITI() {
         System.out.println("Hello, ITI!");
     }
+    public static void printHelloGit() {
+        System.out.println("Hello, Git!");
+    }
 }
